bb190c2ce6421c9929353f54bb2e14ad230da1b6
shashi9769699589 <shashi9769699589@gmail.com>
2025-12-15 18:06:12 +0530

    showreel3

---
+++ story_content/triggers.js
@@ -0,0 +1,143 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5YzIjFwPmU4":
+        Script1();
+        break;
+      case "6hwfaWIQI1X":
+        Script2();
+        break;
+      case "6iJB1L7x1sz":
+        Script3();
+        break;
+      case "66aYMrs32EB":
+        Script4();
+        break;
+      case "6P6KgOJbokH":
+        Script5();
+        break;
+      case "5uxSi4E1HqN":
+        Script6();
+        break;
+      case "65HbhAAi9Tp":
+        Script7();
+        break;
+      case "6ExlPfLfmeF":
+        Script8();
+        break;
+      case "5tnHvJkPQ9W":
+        Script9();
+        break;
+      case "5bVz98Anoua":
+        Script10();
+        break;
+      case "6S8lT0PL204":
+        Script11();
+        break;
+      case "6NSO1kNxJs1":
+        Script12();
+        break;
+      case "6Qoq92H1B3C":
+        Script13();
+        break;
+      case "5wO6XHe0MHS":
+        Script14();
+        break;
+      case "5kK69aYsuZA":
+        Script15();
+        break;
+      case "6J0nAPAmmk3":
+        Script16();
+        break;
+      case "6kJAgBpozvD":
+        Script17();
+        break;
+      case "5WeqGxcpZx6":
+        Script18();
+        break;
+      case "5YZEUgiqfXh":
+        Script19();
+        break;
+      case "6X95BWizJOO":
+        Script20();
+        break;
+      case "63NB5pvFy4s":
+        Script21();
+        break;
+      case "5gkTsJuS6tz":
+        Script22();
+        break;
+      case "6iiGBPICXcw":
+        Script23();
+        break;
+      case "5gCFL1mVfjy":
+        Script24();
+        break;
+      case "6qUUQPdHj1s":
+        Script25();
+        break;
+      case "6qGI88KHcFb":
+        Script26();
+        break;
+      case "6i4zVOO2dOa":
+        Script27();
+        break;
+      case "5WF3X0j6tbD":
+        Script28();
+        break;
+      case "6cSVfllNwYI":
+        Script29();
+        break;
+      case "5sKjKS3RBy3":
+        Script30();
+        break;
+      case "6lQQZx1qMhQ":
+        Script31();
+        break;
+      case "6e3VHHUwuwr":
+        Script32();
+        break;
+      case "5fAVcrfSXGR":
+        Script33();
+        break;
+      case "5qHtth4r0LX":
+        Script34();
+        break;
+      case "5ZFlTHPAkW6":
+        Script35();
+        break;
+      case "6FrjBt8hIl4":
+        Script36();
+        break;
+      case "5tbiBBpRnZ5":
+        Script37();
+        break;
+      case "660IVfuHXYA":
+        Script38();
+        break;
+      case "5anlsJ8pm3e":
+        Script39();
+        break;
+      case "6ELUq5AdVbL":
+        Script40();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ story_content/user.js
@@ -0,0 +1,456 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script2 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script3 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script4 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script5 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script6 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script7 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script8 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script9 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script10 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script11 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script12 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script13 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script14 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script15 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script16 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script17 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script18 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script19 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script20 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script21 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script22 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script23 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script24 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script25 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script26 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script27 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script28 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script29 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script30 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script31 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script32 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script33 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script34 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script35 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script36 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script37 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script38 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script39 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+window.Script40 = function()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+if (appState.onToggleVolume === 0){
+appState.onToggleVolume(1);
+}
+else{
+appState.onToggleVolume(0);
+}
+}
+
+};
